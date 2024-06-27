dddd29bf7922e7b55b937ff0d1cea6f695d94700
MMMJB <michaelbeck445@gmail.com>
2024-06-26 21:50:17 -0400

    feat: create first iteration of monologue component

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,12 +1,26 @@
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
+import Monologue from "./components/Monologue";
+import Lottie from "react-lottie";
+
+import animation from "./animations/New_Lottie.json";
 
 export default function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
     <div className="mx-auto w-full max-w-screen-lg">
       <Nav />
       <Hero />
-      <div className="h-screen w-full rounded-2xl bg-white" />
+      <Monologue />
+      {/* <Lottie options={defaultOptions} height={400} /> */}
     </div>
   );
 }
--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -5,14 +5,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { LaunchedProvider } from "./dist";
 
+import type { Config } from "./dist";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
+const config: Config = {
+  toolbarOptions: {
+    className: "!static",
+  },
+  locked: true,
+};
+
 root.render(
   // <React.StrictMode>
-  // <LaunchedProvider>
-  <App />,
-  // </LaunchedProvider>,
+  <LaunchedProvider config={config}>
+    <App />
+  </LaunchedProvider>,
   // </React.StrictMode>
 );
