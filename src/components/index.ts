4d302492fab65e8458c2024ad07407fe1257d78e
MMMJB <michaelbeck445@gmail.com>
2024-07-13 13:35:14 -0400

    refactor: export HTMLTextTags type

--- src/components/index.ts
+++ src/components/index.ts
@@ -12,6 +12,17 @@ export type LaunchedComponentProps<
   element?: c;
 };
 
+export type HTMLTextTags =
+  | "p"
+  | "span"
+  | "div"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6";
+
 export type HTMLTagsWithoutChildren =
   | "img"
   | "input"
