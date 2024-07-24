44050bcd91ddfa8e7d1fc61af32af6ec0798ff80
MMMJB <michaelbeck445@gmail.com>
2024-07-23 21:03:45 -0400

    build: update build script for linux machines

--- demo/vercel.sh
+++ demo/vercel.sh
@@ -5,9 +5,9 @@ SEARCH_DIR="./src"
 # Find all .ts and .tsx files in the specified directory and its subdirectories
 find "$SEARCH_DIR" -type f \( -name "*.ts" -o -name "*.tsx" \) | while read -r file; do
     # Replace "./dist/components" or "../dist/components" within double quotes with "launched/components"
-    sed -i '' -E 's@(\"\.\/dist|\"\.\./dist)/components"@\"launched/components"@g' "$file"
+    sed -i'' -E 's@(\"\.\/dist|\"\.\./dist)/components"@\"launched/components"@g' "$file"
     # Replace any "./dist" or "../dist" within double quotes with "launched", regardless of what follows
-    sed -i '' -E 's@(\"\.\/dist|\"\.\./dist)(/[^ ]*)?"@\"launched"@g' "$file"
+    sed -i'' -E 's@(\"\.\/dist|\"\.\./dist)(/[^ ]*)?"@\"launched"@g' "$file"
 done
 
 echo "Replacement complete."
