ec15c865627bfce1f261bf2829decc998a01566b
MMMJB <michaelbeck445@gmail.com>
2024-08-14 16:39:15 -0400

    feat: update demo navigation to reflect new docs

--- demo/src/components/Nav.tsx
+++ demo/src/components/Nav.tsx
@@ -1,7 +1,6 @@
 import { useRef, useEffect } from "react";
 import { useWindowSize } from "usehooks-ts";
 
-// import { ChevronDown } from "react-feather";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
@@ -9,14 +8,17 @@ function NavItem({
   children,
   href,
   className,
+  external,
 }: {
   children: React.ReactNode;
   href: string;
   className?: string;
+  external?: boolean;
 }) {
   return (
     <a
       href={href}
+      target={external ? "_blank" : "_self"}
       className={`text-sm text-text-secondary hover:underline ${className}`}
     >
       {children}
@@ -50,20 +52,12 @@ export default function Nav() {
       className="fixed bottom-0 left-1/2 z-[1000] flex w-full -translate-x-1/2 items-center justify-center gap-10 bg-bg py-5 pl-10 pr-5 transition-colors ease-out sm:bottom-auto sm:top-0 sm:bg-transparent"
     >
       <nav className="flex items-center gap-5">
-        {/* <svg
-          viewBox="0 0 48 48"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-        >
-          <circle cx="24" cy="24" r="24" fill="#FF6924" />
-          <circle cx="23.9999" cy="18.1224" r="18.1224" fill="#FF7F24" />
-          <circle cx="24.0001" cy="10.7755" r="10.7755" fill="#FF9524" />
-        </svg> */}
-        <NavItem href="/">Docs</NavItem>
-        <NavItem href="/">Guides</NavItem>
-        <NavItem href="/">Contribute</NavItem>
-        <NavItem href="/">
+        <NavItem href="https://docs.launched.tech/">Docs</NavItem>
+        <NavItem href="https://docs.launched.tech/Guides">Guides</NavItem>
+        <NavItem external href="https://github.com/launchsite-tech/launched">
+          Contribute
+        </NavItem>
+        <NavItem href="https://docs.launched.tech/Get%20Started/02%20quickstart">
           <button className="btn">Get started</button>
         </NavItem>
       </nav>
