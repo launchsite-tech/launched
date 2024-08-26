5bb6a034d21340b8b4c89ed6bd02a0112fc56ff7
MMMJB <michaelbeck445@gmail.com>
2024-08-25 21:09:09 -0400

    docs: add TSDoc annotations to error.ts

--- src/core/utils/error.ts
+++ src/core/utils/error.ts
@@ -1,7 +1,21 @@
+/**
+ * @internal
+ *
+ * Generate an error message.
+ *
+ * @param msg - The error message
+ */
 export function generateError(msg: string): string {
   return `[𖣠] Launched error: ${msg}`;
 }
 
+/**
+ * @internal
+ *
+ * Throw an error.
+ *
+ * @param msg - The error message
+ */
 export default function error(msg: string): never {
   throw new Error(generateError(msg));
 }
