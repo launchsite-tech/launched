3ab1fb2a188ee40ad34d6817931d7fecffd61584
MMMJB <michaelbeck445@gmail.com>
2024-08-25 21:12:18 -0400

    docs: add TSDoc annotations to tagToValues.ts

--- src/core/utils/tagToValues.ts
+++ src/core/utils/tagToValues.ts
@@ -1,6 +1,17 @@
 import flattenTagValue from "./flattenTagValue.js";
 import type { Tag, TagData } from "../context.js";
 
+/**
+ * @internal
+ *
+ * Transform a tag into its flattened values.
+ *
+ * @param tag - The tag to transform
+ *
+ * @returns The tag's values
+ *
+ * @see {@link flattenTagValue}
+ */
 export default function tagToValues(tag: Tag): TagData["value"] {
   if (Array.isArray(tag.data.value)) {
     return tag.data.value.map((t) =>
